32b95012fb6b4b62f49266fb4f1323f72424a129
lulu <lucas.verbeiren@gmail.com>
2025-04-19 11:34:31 +0200

    constructing a node

--- app/src/main/java/org/example/Node.java
+++ app/src/main/java/org/example/Node.java
@@ -4,8 +4,15 @@ import javafx.util.Pair;
 
 public class Node {
 
-    private int id;
-    private String name;
     private Pair<Double, Double> coords;
+    private String name;
+    private int id;
+
+
+    public Node(int id, String name, Pair<Double, Double> coords) {
+        coords = coords; 
+        name = name; 
+        id = id; 
+    }
 
 }
