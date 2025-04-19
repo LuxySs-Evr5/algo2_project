67b0fa000cbbcc258ed1c93fc6f370bd5f34d6b4
lulu <lucas.verbeiren@gmail.com>
2025-04-19 11:40:35 +0200

    renamed App to Main

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import javafx.util.Pair;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
         Node node = new Node(0, "coucou", new Pair<Double, Double>(0.0, 0.0));
     }
